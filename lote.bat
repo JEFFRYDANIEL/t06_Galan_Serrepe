9de1f77ae6811d709f6cb23d49dfaadaedf90f4b
JEFFRYDANIEL <jgalansenmache@gmail.com>
2019-11-17 15:10:00 -0500

    ejercicios de trabajo nro 6

---
+++ lote.bat
@@ -0,0 +1,90 @@
+python galan/ejercicio1.py 54.9 43.9 34.5 45.6
+python galan/ejercicio1.py 23.8 45.8 67.7 33.1
+python galan/ejercicio1.py 34.6 12.6 22.4 45.7
+python galan/ejercicio2.py 45 34 12
+python galan/ejercicio2.py 34 30 10
+python galan/ejercicio2.py 65 49 20
+python galan/ejercicio3.py 20 24 25
+python galan/ejercicio3.py 20 30 61
+python galan/ejercicio3.py 34 38 46
+python galan/ejercicio4.py 16 19 44
+python galan/ejercicio4.py 30 35 36
+python galan/ejercicio4.py 10 45 32
+python galan/ejercicio5.py 12.4 20.7 18.6
+python galan/ejercicio5.py 20.4 23.4 30.6
+python galan/ejercicio5.py 34.5 32.9 40.5
+python galan/ejercicio6.py 12.3 10.2 11.3
+python galan/ejercicio6.py 16.6 17.7 18.8
+python galan/ejercicio6.py 23.3 34.5 36.6
+python galan/ejercicio7.py 10 11 12
+python galan/ejercicio7.py 20 25 30
+python galan/ejercicio7.py 40 44 47
+python galan/ejercicio8.py 12 17 13
+python galan/ejercicio8.py 34 32 31
+python galan/ejercicio8.py 23 65 10
+python galan/ejercicio9.py 22 24 22
+python galan/ejercicio9.py 55 34 22
+python galan/ejercicio9.py 65 10 22
+python galan/ejercicio10.py 20 80 49 23
+python galan/ejercicio10.py 23 49 22 20
+python galan/ejercicio10.py 12 23 22 11
+python galan/ejercicio11.py 33.3 42.2 42.2 50.4
+python galan/ejercicio11.py 22.4 12.3 22.1 20.3
+python galan/ejercicio11.py 35.3 45.5 60.4 30.4
+python galan/ejercicio12.py 45 34 12
+python galan/ejercicio12.py 34 30 10
+python galan/ejercicio12.py 65 49 20
+python galan/ejercicio13.py 20 24 25
+python galan/ejercicio13.py 20 30 61
+python galan/ejercicio13.py 34 38 46
+python galan/ejercicio14.py 16 19 44
+python galan/ejercicio14.py 30 35 36
+python galan/ejercicio14.py 10 45 32
+python galan/ejercicio15.py 12.4 20.7 18.6
+python galan/ejercicio15.py 20.4 23.4 30.6
+python galan/ejercicio15.py 34.5 32.9 40.5
+python galan/ejercicio16.py 12.3 10.2 11.3
+python galan/ejercicio16.py 16.6 17.7 18.8
+python galan/ejercicio16.py 23.3 34.5 36.6
+python galan/ejercicio17.py 10 11 12
+python galan/ejercicio17.py 20 25 30
+python galan/ejercicio17.py 43 44 55
+python galan/ejercicio18.py 12 17 13
+python galan/ejercicio18.py 34 32 31
+python galan/ejercicio18.py 23 65 10
+python galan/ejercicio19.py 22 24 22
+python galan/ejercicio19.py 55 34 22
+python galan/ejercicio19.py 65 10 22
+python galan/ejercicio20.py 20 80 49 23
+python galan/ejercicio20.py 23 49 22 20
+python galan/ejercicio20.py 12 23 22 11
+python galan/ejercicio21.py 83.3 82.2 52.2 50.4
+python galan/ejercicio21.py 42.4 52.3 52.1 90.3
+python galan/ejercicio21.py 67.3 45.5 60.4 30.4
+python galan/ejercicio22.py 45 34 12
+python galan/ejercicio22.py 34 30 10
+python galan/ejercicio22.py 65 49 20
+python galan/ejercicio23.py 20 24 25
+python galan/ejercicio23.py 20 30 61
+python galan/ejercicio23.py 34 38 46
+python galan/ejercicio24.py 16 19 44
+python galan/ejercicio24.py 30 35 36
+python galan/ejercicio24.py 10 45 32
+python galan/ejercicio25.py 12.4 20.7 18.6
+python galan/ejercicio25.py 20.4 23.4 30.6
+python galan/ejercicio25.py 34.5 32.9 40.5
+python galan/ejercicio26.py 12.3 10.2 11.3
+python galan/ejercicio26.py 16.6 17.7 18.8
+python galan/ejercicio26.py 23.3 34.5 36.6
+python galan/ejercicio27.py 10 11 12
+python galan/ejercicio27.py 20 25 30
+python galan/ejercicio27.py 43 44 55
+python galan/ejercicio28.py 12 17 13
+python galan/ejercicio28.py 34 32 31
+python galan/ejercicio28.py 23 65 10
+python galan/ejercicio29.py 22 24 22
+python galan/ejercicio29.py 55 34 22
+python galan/ejercicio29.py 65 10 22
+python galan/ejercicio30.py 20 80 49 23
+python galan/ejercicio30.py 70 49 52 50
+python galan/ejercicio30.py 80 93 104 61
---
+++ serrepe/ejercicio11.py
@@ -0,0 +1,19 @@
+import os
+#declarar valores
+nota1,nota2,nota3,promedio=0.0,0.0,0.0,0.0
+
+#pedir variables via argumentos
+nota1=float(os.sys.argv[1])
+nota2=float(os.sys.argv[2])
+nota3=float(os.sys.argv[3])
+promedio=(nota1+nota2+nota3)/3
+
+#validacion
+aprobado=(promedio>10.5)
+
+#condicional doble
+if(aprobado==True):
+    print("APROBADO¡")
+else:
+    print("DESAPROBADO")
+#fin_if
